eba788be83e893378c95d6b7f02a4f2eb4190882
nguyenquangduy <151936156+Subzerozzz@users.noreply.github.com>
2025-10-20 16:34:59 +0700

    commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
+//Thiết lập views
+app.set("views", path.join(__dirname, "views"));
+// Set template engine
+app.set("view engine", "pug");
+
+// Thiết lập thư mục tĩnh
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello Bro!");
+  res.render("client/pages/home", {
+    pageTitle: "Trang chủ",
+    message: "Trang chủ",
+  });
+});
+
+app.get("/tour-list", (req, res) => {
+  res.render("client/pages/tour-list", {
+    pageTitle: "Danh sách tour",
+    message: "Danh sách tour",
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Dự án đang chạy trên cổng ${port}`);
 });
