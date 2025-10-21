65b344ec0380ad448151f1eb550f889515f70ead
nguyenquangduy <151936156+Subzerozzz@users.noreply.github.com>
2025-10-21 11:53:01 +0700

    tách layout

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("client/pages/home", {
     pageTitle: "Trang chủ",
-    message: "Trang chủ",
   });
 });
 
 app.get("/tour-list", (req, res) => {
   res.render("client/pages/tour-list", {
     pageTitle: "Danh sách tour",
-    message: "Danh sách tour",
   });
 });
 
 app.listen(port, () => {
   console.log(`Dự án đang chạy trên cổng ${port}`);
 });
+
+// nguyenduyntn112004/8BXqQPHORJhRzeeI
